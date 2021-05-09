610563e136de079895bd5410b4b4274da416da5f
Samiya Jabbar <samiya.programming@gmail.com>
2021-05-09 16:55:42 +0500

    work completed

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
-from dialogflow_fulfillment import QuickReplies, WebhookClient
+from dialogflow_fulfillment import QuickReplies, WebhookClient, Payload
 from flask import Flask, request, Response, jsonify , make_response
-#from googleapiclient.discovery import build
-#from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 import json
 
 app = Flask(__name__)
 
-"""
-SERVICE_ACCOUNT_FILE = 'keys-food.json'
+SERVICE_ACCOUNT_FILE = 'google-sheet-key.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 creds = None
 creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1DxYub7bbxV_h2AR2lAp8aaTyuy52bmKNsKY8nPTHCnw'
+SAMPLE_SPREADSHEET_ID = '1IYNYC65qKSSQQiPM2lZs0NN9v9SZhNt2FwdnAvJ2Vhw'
 service = build('sheets', 'v4', credentials=creds)
 
-"""
 def handler(agent: WebhookClient) :
     """Handle the webhook request.."""
 
@@ -27,40 +24,49 @@ def handler(agent: WebhookClient) :
         agent.add('I am the chatbot of this page. Ready to assist you with anything you need. What would you like to do?')
         agent.add(QuickReplies(quick_replies=['About us','Schedule a repair','Check your order status','Call us','Talk to a live agent']))
 
-    if intent_name == 'About us':
+    if intent_name == 'About_us':
+        agent.add("Hello, visit our website")
         agent.add('https://atservicefl.com/#about')
-
+    
     if intent_name == 'Schedule_a_repair':
-        link1 = 'https://atservicefl.com/appointment-schedule/'
         agent.add('To schedule an appointment please use the form below and we will be in touch shortly')
-        agent.add(link1)
+        agent.add('https://atservicefl.com/appointment-schedule/')
 
     if intent_name == 'Check_your_order_status':
-        link2 = 'https://atservicefl.com/order-status//'
         agent.add('To get a quick update on your order please use the form below')
-        agent.add(link2)
+        agent.add('https://atservicefl.com/order-status//')
 
     if intent_name == 'call_us':
+        agent.add("Hello, Contact us on")
         agent.add('(754) 202-2000') 
 
     if intent_name == 'Talk_to_a_live_agent':
-        agent.add('Please  give a heads up on what you would like to chat about, by typing it below')
+        agent.add('Please give a heads up on what you would like to chat about, by typing it below')
 
     if intent_name == 'user_input':
-        user_input = req.get('queryResult').get('queryText')
-        agent.add('Please enter your name and phone # in case we get disconnected')
-        print(user_input)
-
+        global user_concern
+        agent.add('In case we get disconnected, kindly give me your name?')
+        user_concern = agent.context.get('awaiting_user_info').get('parameters').get('any')[0]
+        
     if intent_name == 'name_number':
-        name = req.get('queryResult').get('parameters').get('person').get('name')
-        number = req.get('queryResult').get('parameters').get('phone-number')
+        name = req.get('queryResult').get('parameters').get('any')
+        number = req.get('queryResult').get('parameters').get('phone-number')        
+        from datetime import datetime, date
+        today = date.today()
+        date = today.strftime("%b-%d-%Y")
+        now = datetime.now()
+        time = now.strftime("%H:%M:%S")
+        sheett= [[date, time, user_concern, name, number]]
+        sheet = service.spreadsheets()
+        result = sheet.values().append(spreadsheetId=SAMPLE_SPREADSHEET_ID,range="statements",valueInputOption="USER_ENTERED", body={"values" : sheett}).execute()
+        agent.add("dummy")
+        agent.set_followup_event("follow_up_agent")
+
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     """Handle webhook requests from Dialogflow."""
-    # Get WebhookRequest object
     req = request.get_json(force=True)
-    # Handle request
     agent = WebhookClient(req)
     agent.handle_request(handler)
     return agent.response
